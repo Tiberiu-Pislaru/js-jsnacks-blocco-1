c68680a41ab54cff32babaa5457d6543f98bbad5
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-15 14:31:32 +0100

    Snack 1

---
+++ snack-1/js/script.js
@@ -0,0 +1,13 @@
+// SNACK 1
+// 1 creiamo una variabile (sum) che avrà il valore 0 inizialmente
+let sum= 0;
+// 2 creiamo il ciclo for con il contattore che deve arrivare fino a 10, partendo da 0
+//  al suo interno chiediamo all'utente di dirci un numero,
+// che sarà trasformato in un 'int' con parseInt
+// poi aggiungiamo alla variabile sum il valore che ci fornirà l'utente
+for(let i =0; i<10; i++){
+    const num =parseInt(prompt('Dimmi un numero'));
+    sum += num;
+}
+// 3 stampiamo in console la somma dei numeri che l'utente ci ha dato
+console.log(sum)
