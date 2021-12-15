633a69ecbdc56a5661d9f42bc07f93b1c30854ed
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-15 14:46:13 +0100

    Snack 2

---
+++ snack-2/js/script.js
@@ -0,0 +1,11 @@
+// SNACK 2
+// 1 chiediamo all'utente di fornirci un numero che sarà salvato in una variabile
+const num = parseInt(prompt('Può fornirci un numero?'));
+
+// 2 tramite un for calcoliamo il cubo dei primi N numeri
+// successivamente lo stampiamo in console
+for (let i = 1; i <= num; i++) {
+    const cubo = Math.pow(i, 3);
+    console.log(cubo);
+    
+}
