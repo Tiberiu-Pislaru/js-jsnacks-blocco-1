5342df81a49de8f6512151992de6b745402cdb21
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-16 10:08:37 +0100

    Upgrade

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,6 +1,16 @@
 // SNACK 3
-for (let i = 1; i <= 1000; i++) {
+// for (let i = 1; i <= 1000; i++) {
     
-    console.log('La potenza è di: ',Math.pow(i, 2));
+//     console.log('La potenza è di: ',Math.pow(i, 2));
     
+// }
+
+let exp= 1;
+let max = 1000;
+let num= 2;
+
+while(num ** exp < max){
+    console.log(Math.pow(num, exp));
+    exp++;
+
 }
